f808db7cb6319f399d45629bcf1f4abfa3d2c7f6
DiegoPro <despinosag@outlook.com>
2022-08-03 23:43:16 -0500

    Boton para traer todos y traer uno con error

--- src/App.js
+++ src/App.js
@@ -29,12 +29,38 @@ function App() {
 
   }
 
+  const obtenerAllArchivos=async()=>{
+
+    await axios.get("http://localhost:4000/getAll")
+    .then(response=>{
+      console.log(response.data.values);      
+    }).catch(error=>{
+      console.log(error);
+    })
+
+  }
+
+  const obtenerArchivo=async()=>{
+
+    await axios.get("http://localhost:4000/get/uploads/ce688cb4-6e7c-45fc-a55a-6a1ed83a417e-CatalogoPartesXG750_2015.pdf")
+    .then(response=>{
+      console.log(response.data);
+    }).catch(error=>{
+      console.log(error);
+    })
+
+  }
+
   return (
     <div className="App">
       <br/><br/>
       <input type="file" name='files' multiple onChange={(e)=>subirArchivos(e.target.files)}/>
       <br/><br/>
       <button className='btn btn-primary' onClick={()=>insertarArchivos()}>Insertar Archivos</button>
+      <br/><br/>
+      <button className='btn btn-success' onClick={()=>obtenerAllArchivos()}>Obtener all Archivos</button>
+      <br/><br/>
+      <button className='btn btn-success' onClick={()=>obtenerArchivo()}>Obtener Archivo</button>
     </div>
   );
 }
